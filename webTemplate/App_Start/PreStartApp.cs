e716fe695eac8a1ae1b895208e0b73a08239157f
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-26 21:02:30 +0200

    Add WebActivator (use PreStartApp.cs)

---
+++ webTemplate/App_Start/PreStartApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webTemplate.App_Start;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(PreStartApp), "Start")]
+namespace webTemplate.App_Start
+{
+    public class PreStartApp
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// The method is started once before the start of the application
+        /// </summary>
+        public static void Start()
+        {
+            logger.Info("Application PreStart");
+        }
+    }
+}
