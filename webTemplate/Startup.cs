0f9c5f8fc22269e0547a6f7d0d663c418e884684
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-26 16:41:24 +0200

    Add project files.

---
+++ webTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webTemplate.Startup))]
+namespace webTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
